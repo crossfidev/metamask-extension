8d3e474c923d87b6868743bf90d2cd69d66148d5
Mark Stacey <markjstacey@gmail.com>
2024-01-22 11:00:40 -0330

    refactor: Replace usages of the deprecated `setProviderType`
    All usages of the deprecated `NetworkController` method
    `setProviderType` have been replaced. The method `setActiveNetwork` is
    now used instead; it was updated in a recent release to accept types as
    well as IDs.

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3130,9 +3130,6 @@ export default class MetamaskController extends EventEmitter {
       verifyPassword: this.verifyPassword.bind(this),
 
       // network management
-      setProviderType: (type) => {
-        return this.networkController.setProviderType(type);
-      },
       setActiveNetwork: (networkConfigurationId) => {
         return this.networkController.setActiveNetwork(networkConfigurationId);
       },
